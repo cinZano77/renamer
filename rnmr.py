556eb814b5f3009ed1c2a829876c00f9f8408b23
yaroslav_korenskoy <41763970+yaroslavkorenskoy@users.noreply.github.com>
2019-10-23 04:15:13 +0400

    Update rnmr.py

--- rnmr.py
+++ rnmr.py
@@ -3,7 +3,7 @@ import os
 
 for unit in glob.glob("/Users/UserName/YourDirectory/SomeFolder/**/*", recursive=True):
     if 'line we need to change ' in unit:
-        print(unit)
+        print(unit)  # this line – just to see in a log what the script founds in the folder
         os.rename(unit, unit.replace('line we need to change ', 'new line (usually left empty)'))
 
 # We put /**/* to scan through an every folder and a file inside them
